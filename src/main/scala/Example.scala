6e5beac1f606108388c7c20db8872b8caeadb0be
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2020-03-04 20:27:13 -0300

    style: format code

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -22,21 +22,24 @@ object Example {
     BasicReactor
       .linearRamp(-200, 200, 1)
       .tickRate(20.millis)
-      .render{x =>
+      .render { x =>
         val y = x.degrees.sin * 200
         val planet = Image.circle(20.0).noStroke.fillColor(Color.seaGreen)
-        val moon = Image.circle(5.0).noStroke.fillColor(Color.slateGray).at((x * 10).degrees.cos * 50, (x * 10).degrees.sin * 50)
+        val moon = Image
+          .circle(5.0)
+          .noStroke
+          .fillColor(Color.slateGray)
+          .at((x * 10).degrees.cos * 50, (x * 10).degrees.sin * 50)
 
         moon.on(planet).at(x, y)
       }
 
   val frame = Frame.size(600, 600)
 
-
   def main(args: Array[String]): Unit = {
-    image.draw()
+    // image.draw()
 
     // Comment out the above and uncomment the below to display the animation
-    // animation.run(frame)
+    animation.run(frame)
   }
 }
